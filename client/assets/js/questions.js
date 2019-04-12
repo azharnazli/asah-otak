0d1ff5cf9d9c44ac2316c04e7ce78333edbd7fe2
mhmmd-ysf <muhammad_yusuf22@outlook.co.id>
2019-04-12 14:59:50 +0700

    Client final

--- client/assets/js/questions.js
+++ client/assets/js/questions.js
@@ -1,10 +1,43 @@
-Vue.component('questions-what', {
-  data: function() {
+Vue.component('questionscomponent', {
+  props: ['questions'],
+  data() {
     return {
-      coba: `['tes 1', 'tes 2']`
+      answers: {
+        0: '',
+        1: '',
+        2: '',
+        3: '',
+        4: '',
+        5: '',
+        6: '',
+        7: '',
+        8: '',
+        9: '',
+      },
+    };
+  },
+  methods: {
+    submission() {
+      // console.log('answers')
+      // console.log(this.answers)
+      this.$emit('submit-questions', this.answers)
     }
   },
   template: `
-    <h1>Test {{coba}}</h1>
+  <form v-on:submit.prevent="submission">
+    <div v-for="(item, index) in questions" v-bind:item="item">
+      <h3>{{item.question}}</h3>
+      <div style="margin-left: 30px;">
+        <div v-for="(shuffledAnswer, shuffledAnswerIndex) in item.shuffledAnswers">
+          <input type="radio" v-bind:name="item.index" v-bind:value="shuffledAnswer" v-model="answers[index]"> {{shuffledAnswer}}<br>
+        </div>
+      </div>
+      <br>
+    </div>
+    <button class="btn btn-primary" type="submit" >Submit</button>
+  </form>
   `
 })
+
+
+// <form><input type="radio" name="answer" value="0"> {{item.correct_answer}}</form>
